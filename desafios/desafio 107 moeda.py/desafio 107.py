2b72bf4378d4f2f36073294f9bce6bfca5783ccd
joao <jvmmoraesdev@gmail.com>
2025-08-24 23:50:34 -0300

    desafio 107 adicionado

---
+++ desafios/desafio 107 moeda.py/desafio 107.py
@@ -0,0 +1,8 @@
+import modulo_moeda
+
+n = 80
+
+print(f"O dobro de {n} é {modulo_moeda.dobrar(n)}")
+print(f"A metade de {n} é {modulo_moeda.metade(n)}")
+print(f"Um aumento de 17% em {n} é {modulo_moeda.aumento(n,17)}")
+print(f"Um desconto de 17% em {n} é {modulo_moeda.desconto(n,17):.2f}")
---
+++ desafios/desafio 107 moeda.py/modulo_moeda.py
@@ -0,0 +1,12 @@
+def dobrar(n):
+    return 2 * n
+
+def metade(n):
+    return n / 2
+
+def aumento(n, t):
+    return n * (1 + t / 100)
+
+def desconto(n , t):
+    return n * (1 - t / 100)
+
