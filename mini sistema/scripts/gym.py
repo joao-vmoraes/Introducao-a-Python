6a1ae6082e9139d6679529608374f4519639ab88
joao <jvmmoraesdev@gmail.com>
2025-08-27 00:54:42 -0300

    vou mudar de arquivos.txt para arquivos no formato json, mas tenho que estudar um poucoi sobre antes

--- mini sistema/scripts/gym.py
+++ mini sistema/scripts/gym.py
@@ -1,18 +1,26 @@
 import modulos as md
 import os
 
+treinos = ["Costas" , "Peito" , "Perna"]
 opcoes_menu_principal = ["Abrir Treino" , "Adicionar Treino" , "Excluir Treino" , "Sair do Sistema"]
 
 
-opcao = md.menu_principal(opcoes_menu_principal)
 
 while True:
+    opcao = md.menu_principal(opcoes_menu_principal)
+
     if opcao == 1:
+        md.listar_treinos(treinos)
+        resposta = md.escolher_treino()
         continue#abrir treino
+
     elif opcao == 2:
         continue#adicionar treino
+
     elif opcao == 3:
         continue#excluir treino
+
     elif opcao == 4:
         break#sair do sistema
+
 
--- mini sistema/scripts/modulos.py
+++ mini sistema/scripts/modulos.py
@@ -1,5 +1,6 @@
 from time import sleep
 import os
+import datetime
 
 def linha(tam=40):
     print("=" * tam)
@@ -25,18 +26,19 @@ def menu_principal(list):
             print("ERRO: Tipo de dado enviado não válido.")
             sleep(2)
 
-def listar_treinos():
-    caminho_do_script = os.path.abspath(__file__) #pega o caminho completo do arquivo
-    caminho_da_pasta_do_script = os.path.dirname(caminho_do_script) #pega o caminho da pasta onde o script esta
-    caminho_da_pasta_acima = os.path.dirname(caminho_da_pasta_do_script) # pega o caminho da pasta que contem a pasta do script
-    pasta_treinos = os.path.join(caminho_da_pasta_acima, "treinos") #recebe o caminho da pasta 'treinos'
-
-    lista_treinos = os.listdir(pasta_treinos) #adiciona todos os arquivos em uma lista
-
+def listar_treinos(list):
     cabecalho("Lista de Treinos")
 
     c = 1
-    for i in lista_treinos:
-        print(f"{c} - {os.path.splitext(i)[0]}") #funcao para nao sair na impressao o tipo do arquivo
+    for i in list:
+        print(f"{c} - {i}") #funcao para nao sair na impressao o tipo do arquivo
         c += 1
 
+def escolher_treino():
+    while True:
+        try:
+            resposta = int(input("Digite a opção >>> "))
+            return resposta
+        except:
+            print("ERRO: Tipo de dado enviado não válido.")
+            sleep(2)
