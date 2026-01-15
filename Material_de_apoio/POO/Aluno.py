b036970a936aa4e796e66e574be1610f4ae50797
João Moraes <jvmmoraesdev@gmail.com>
2026-01-14 23:57:42 -0300

    adicionado parametros no construtor

---
+++ Material_de_apoio/POO/Aluno.py
@@ -0,0 +1,25 @@
+class Aluno: # declaração da classe
+
+    def __init__(self, nome, idade): #metodo construtor
+        self.nome = f"{nome}" # atributo de instancia
+        self.idade = idade # atributo de instancia
+
+    # Métodos
+
+    def aniversario(self):
+        self.idade += 1
+        print(f"Agora voce tem {self.idade} anos!")
+
+    def boasVindas(self):
+        print(f"Ola {self.nome} ! Seja bem vindo!!!")
+
+# Main
+
+a1 = Aluno("Luiz", 20)
+a2 = Aluno("Pedro", 10)
+
+a1.aniversario()
+a2.aniversario()
+
+a1.boasVindas()
+a2.boasVindas()
