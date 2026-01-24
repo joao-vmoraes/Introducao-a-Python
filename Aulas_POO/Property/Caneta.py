bc60565aeafe743c98b42f64592105a54aab9d48
João Moraes <jvmmoraesdev@gmail.com>
2026-01-24 17:59:04 -0300

    Refactor: Clean code

--- Aulas_POO/Property/Caneta.py
+++ Aulas_POO/Property/Caneta.py
@@ -1,25 +1,17 @@
 class Caneta:
     def __init__(self, cor):
-        self._cor_tinta = cor
+        self.cor = cor
 
     @property # Funciona como se fosse um getter para um atributo da classe.
     def cor(self):
-        return self._cor_tinta
+        return self._cor
 
     @cor.setter
     def cor(self, cor):
-        print("ESTOU NO SETTER ", cor)
+        print("ESTOU NO SETTER", cor)
         self._cor = cor
 
 
 c1 = Caneta("Azul")
-
 c1.cor = 'Vermelho'
-c1.cor = 'Amarelo'
-
-print(c1.cor)
-print(c1.cor)
-print(c1.cor)
-print(c1.cor)
-print(c1.cor_vidro) #A chamada é feita como se fosse um atributo!!!!
-print(c1.cor_vidro)
+print(c1.cor)#A chamada é feita como se fosse um atributo!!!!
