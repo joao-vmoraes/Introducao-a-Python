367d5bdf1df3f4c3d8a24771ff4b886a8c3666ad
João Moraes <jvmmoraesdev@gmail.com>
2026-01-28 23:20:23 -0300

    feat: Add more dunder methods

--- Aulas_POO/Dunder_methods/dunder.py
+++ Aulas_POO/Dunder_methods/dunder.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 class Ponto:
     def __init__(self, x , y):
         self.x = x
@@ -9,9 +11,24 @@ class Ponto:
     
     def __str__(self): # é o metodo principal quando chamar o print(objeto)
         return f'({self.x} , {self.y})'
+    
+    def __add__(self, other: Ponto) -> Ponto: # adicionar
+        novo_x = self.x + other.x
+        novo_y = self.y + other.y
+        return Ponto(novo_x, novo_y)
+    
+    def __gt__(self, other: Ponto) -> bool: # maior que
+        p1 = self.x + self.y
+        p2 = other.x + other.y
+        return p1 > p2
 
 p1 = Ponto(1,2)
 p2 = Ponto(4,5)
+p3 = p1 + p2
 
 print(p1)
-print(repr(p2))
+print(f'{p2!r}') # repr
+print(p3)
+
+
+print(p1 > p3)
